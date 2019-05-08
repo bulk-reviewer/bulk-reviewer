2545740ced153b2900894ad0bf46a8550b9e9613
Tim Walsh <timothyryanwalsh@gmail.com>
2019-05-08 13:48:55 -0400

    Fix icon on Linux

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import { app, BrowserWindow, Menu, shell } from 'electron'
+const path = require('path')
 
 /**
  * Set `__static` path to static files in production
@@ -19,11 +20,23 @@ function createWindow () {
   /**
    * Initial window options
    */
-  mainWindow = new BrowserWindow({
-    height: 700,
-    useContentSize: true,
-    width: 1100
-  })
+
+  let mainWindow
+
+  if (process.platform === 'darwin') {
+    mainWindow = new BrowserWindow({
+      height: 700,
+      useContentSize: true,
+      width: 1100
+    })
+  } else {
+    mainWindow = new BrowserWindow({
+      height: 700,
+      useContentSize: true,
+      width: 1100,
+      icon: path.join(__dirname, '/icon/256x256.png')
+    })
+  }
 
   mainWindow.loadURL(winURL)
 
