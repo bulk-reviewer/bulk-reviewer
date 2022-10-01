9c56b0dcd4a462e7d02261cf9a5ecb370a9cdc1b
Tessa Walsh <tessa@bitarchivist.net>
2022-09-30 22:27:25 -0400

    Add auto updating

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import { app, BrowserWindow, Menu, shell } from 'electron'
+import { autoUpdater } from "electron-updater"
 
 /**
  * Set `__static` path to static files in production
@@ -73,22 +74,7 @@ app.on('activate', () => {
   }
 })
 
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
-
-/*
-import { autoUpdater } from 'electron-updater'
-
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
-
+// Auto update
 app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdatesAndNotify()
 })
- */
